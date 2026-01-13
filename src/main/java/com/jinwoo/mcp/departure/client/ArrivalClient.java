26b26cda7bacfb185af2050e02fd671c7f2ef3d6
DESKTOP-GO177QK\jinwo <jinwoo2202@naver.com>
2026-01-13 18:39:35 +0900

    fix

--- src/main/java/com/jinwoo/mcp/departure/client/ArrivalClient.java
+++ src/main/java/com/jinwoo/mcp/departure/client/ArrivalClient.java
@@ -1,6 +1,9 @@
 package com.jinwoo.mcp.departure.client;
+import org.springframework.stereotype.Component;
+
 import java.util.*;
 
+@Component
 public interface ArrivalClient {
     List<Integer> getRemainingMinutes(String station, String line, String direction);
 }
